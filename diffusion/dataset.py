615e13fab29101ebeab13c08e33ef755babbd1e8
Vish <vish@bigrat.monster>
2025-10-22 02:23:31 +0000

    feat: puller

---
+++ diffusion/dataset.py
@@ -0,0 +1,11 @@
+"""
+I use two datasets here:
+
+Textures:
+G. J. Burghouts and J. M. Geusebroek, Material-specific adaptation of color invariant features,
+Pattern Recognition Letters, vol. 30, 306-313, 2009.
+
+
+MNIST:
+The mnist dataset idk
+"""
